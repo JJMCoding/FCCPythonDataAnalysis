a9aeb4607c14a73c42f5e4a04d8e8cbbacea0a64
James McKay <91065990+JJMCoding@users.noreply.github.com>
2025-02-22 11:10:57 +0000

    Update main.py

--- Task5/main.py
+++ Task5/main.py
@@ -1,39 +1,9 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats import linregress
-import numpy as np
-def draw_plot():
-    # Read data from file
-    df = pd.read_csv('epa-sea-level.csv')
+# This entrypoint file to be used in development. Start by reading README.md
+import sea_level_predictor
+from unittest import main
 
-    # Create scatter plot
-    y = df['CSIRO Adjusted Sea Level']
-    x = df['Year']
-    plt.scatter(x, y)
+# Test your function by calling it here
+sea_level_predictor.draw_plot()
 
-    plt.show()
-    # Create first line of best fit
-    lineA = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    xA = np.arange(df['Year'].min(),2050,1)
-    yA = xA*lineA.slope + lineA.intercept
-
-    plt.plot(xA,yA)
-
-    # Create second line of best fit
-    df_2000 = df[df['Year'] >= 2000]
-
-    lineB = linregress(df_2000['Year'], df_2000['CSIRO Adjusted Sea Level'])
-    xB = np.arange(2000,2050,1)
-    yB = xB*lineB.slope + lineB.intercept
-
-    plt.plot(xB,yB)
-
-
-    # Add labels and title
-    plt.xlabel('Year')
-    plt.ylabel('Sea Level (inches)')
-    plt.title('Rise in Sea Level')
-    
-    # Save plot and return data for testing (DO NOT MODIFY)
-    plt.savefig('sea_level_plot.png')
-    return plt.gca()
+# Run unit tests automatically
+main(module='test_module', exit=False)
